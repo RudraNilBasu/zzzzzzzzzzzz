34c4c6e7089f740af36da92b952c934fea5523f5
Rudra Nil Basu <rudra@hackerrank.com>
2021-05-13 12:16:56 +0530

    updates

--- App.js
+++ App.js
@@ -5,7 +5,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <Text>This is a new line!</Text>
+      <Text>This is another new line!!!!!!!</Text>
+      <Text>This is a new line!</Text>
+      <Text>This is a new line!</Text>
+      <Text>This is a new line!</Text>
+      <Text>This is a new line!</Text>
+      <Text>This is a new line!</Text>
+      <Text>This is a new line!</Text>
+      <Text>This is a new line!</Text>
+      <Text>Bye :)</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#894',
     alignItems: 'center',
     justifyContent: 'center',
   },
